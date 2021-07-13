d6f3bffc5ddf7f56ad84a1561f66a98d3393c150
SaadAkbar <saadakbar@A006-00583.arbisoft.com>
2021-07-13 11:45:07 +0500

    Current django code

---
+++ mysite/polls/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.urls import path
+from django.http import HttpResponse
+from . import views
+from .models import Question
+
+# Create your views here.
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+urlpatterns = [
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+# Leave the rest of the views (detail, results, vote) unchanged
